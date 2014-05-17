ff6c3871023acfe7136cb3be75cd27ed37e05e4d
synthverity <tinkansinar@gmail.com>
2014-05-16 22:20:40 -0400

    Added main loop and ability to re-roll

--- fiveDice.c
+++ fiveDice.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 
 // Obvious main is obvious
 int main(void)
@@ -10,33 +11,59 @@ int main(void)
 	srand(time(NULL));
 
 	// Basic variables for the programs use
+	int running = 1;
 	int i, j;
 	int dice[5];
+	char input;
 
-	// First for loop to set the random value of the dice
-	for(i = 0; i < 5; i++)
+	// Main game(while) loop
+	while(running)
 	{
-		dice[i] = rand() % 6 + 1;
-	}
-
-	// Second for loop to print out the values of each dice,
-	// Then to compare them to the other dice for matches
-	for(i = 0; i < 5; i++)
-	{
-		printf("Dice %d: %d\t", i+1, dice[i]);
-
-		// Nested for loop to compare the values of the dice
-		for(j = 0; j < 5; j++)
+		// First for loop to set the random value of the dice
+		for(i = 0; i < 5; i++)
 		{
-			// Check to see if the dice match, and that it is not the same dice
-			if(dice[i] == dice[j] && i != j)
-			{
-				printf("Equal to dice %d  ", j+1);
-			}
+			dice[i] = rand() % 6 + 1;
 		}
-		
-		// New line to seperate the dice
-		printf("\n");
+
+		// Second for loop to print out the values of each dice,
+		// Then to compare them to the other dice for matches
+		for(i = 0; i < 5; i++)
+		{
+			printf("Dice %d: %d\t", i+1, dice[i]);
+
+			// Nested for loop to compare the values of the dice
+			for(j = 0; j < 5; j++)
+			{
+				// Check to see if the dice match, and that it is not the same dice
+				if(dice[i] == dice[j] && i != j)
+				{
+					printf("Equal to dice %d  ", j+1);
+				}
+			}
+
+			// New line to keep dice seperate
+			printf("\n");
+		}
+
+		// Ask the player if they want to roll again
+		while(input != 'n' && input != 'y')
+		{
+			printf("\nDo you wish to roll again?[y/N]: ");
+			scanf("%c", &input);
+
+			// Parse input to lowercase
+			input = tolower(input);
+		}
+
+		// React to player input
+		if(input == 'n')
+		{
+			running = 0;
+		}
+		else
+		{
+			input = 0;
+		}
 	}
 
 	// End of program
